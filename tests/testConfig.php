f8d4e37c5f4ae2c4aada40d8d4fe3ff443cde54c
Stefano <Stefano@My-PC>
2013-05-27 23:07:11 +0200

    update db settings for tests

--- tests/testConfig.php
+++ tests/testConfig.php
@@ -7,7 +7,7 @@ defined('TEST_STEFANO_DB_ADAPTER')
 defined('TEST_STEFANO_DB_HOSTNAME')
     || define('TEST_STEFANO_DB_HOSTNAME', '127.0.0.1');
 defined('TEST_STEFANO_DB_DB_NAME')
-    || define('TEST_STEFANO_DB_DB_NAME', 'stefano-tests');
+    || define('TEST_STEFANO_DB_DB_NAME', 'stefano_tests');
 defined('TEST_STEFANO_DB_USER')
     || define('TEST_STEFANO_DB_USER', 'root');
 defined('TEST_STEFANO_DB_PASSWORD')
