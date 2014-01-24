92ec7232b8978684b0025d041c93913ec49e8644
Stefan Bartko <bartko.stefan@gmail.com>
2014-01-24 18:55:34 +0100

    Added some unit tests

---
+++ tests/StefanoTreeTest/Unit/NestedSet/Adapter/Doctrine2DBALAdapterTest.php
@@ -0,0 +1,65 @@
+<?php
+namespace StefanoTreeTest\Unit\NestedSet\Adapter;
+
+use StefanoTree\NestedSet\Adapter\Doctrine2DBALAdapter;
+use StefanoTree\NestedSet\Options;
+
+class Doctrine2DBALAdapterTest
+    extends \PHPUnit_Framework_TestCase
+{
+    protected function tearDown() {
+        \Mockery::close();
+    }
+
+    public function testGetDefaultDbSelect() {
+        $adapter = $this->getAdapter();
+
+        $this->assertEquals('SELECT * FROM tableName',
+            trim($adapter->getDefaultDbSelect()->getSQL()));
+    }
+
+    public function testGetDefaultDbSelectMustAlwaysReturnNewInstance() {
+        $adapter = $this->getAdapter();
+        $this->assertNotSame($adapter->getDefaultDbSelect(), $adapter->getDefaultDbSelect());
+    }
+
+    public function testSetDefaulDbSelect() {
+        $adapter = $this->getAdapter();
+
+        $select = $this->getConnection()
+                       ->createQueryBuilder()
+                       ->select('*')
+                       ->from('someTable', null);
+
+        $adapter->setDefaultDbSelect($select);
+
+        $this->assertEquals($select->getSQL()
+            , $adapter->getDefaultDbSelect()->getSQL());
+    }
+
+    /**
+     * @return Doctrine2DBALAdapter
+     */
+    private function getAdapter() {
+        $options = new Options(array(
+            'tableName'    => 'tableName',
+            'idColumnName' => 'id',
+        ));
+
+        return new Doctrine2DBALAdapter($options, $this->getConnection());
+    }
+
+    /**
+     * @return \Doctrine\DBAL\Connection
+     */
+    private function getConnection() {
+        $config = new \Doctrine\DBAL\Configuration();
+
+        $connectionParams = array(
+            'dbname' => ':memory:',
+            'driver' => 'pdo_sqlite',
+        );
+
+        return  \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+    }
+}
