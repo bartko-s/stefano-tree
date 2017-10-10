e5a94674e6c68e7d1fb6d99050f8b32642d93868
Bartko Stefan <bartko.stefan@gmail.com>
2017-10-10 20:52:58 +0200

    speedup mysql tests

--- tests/StefanoTreeTest/TestUtil.php
+++ tests/StefanoTreeTest/TestUtil.php
@@ -15,6 +15,8 @@ class TestUtil
     private static $zend1DbAdapter;
     private static $doctrine2Connection;
 
+    private static $mysqlDbSchemeCreated = false;
+
     public static function createDbScheme()
     {
         $connection = self::getPDOConnection();
@@ -22,9 +24,11 @@ class TestUtil
         $queries = array();
 
         if ('mysql' == TEST_STEFANO_DB_ADAPTER) {
-            $queries[] = 'DROP TABLE IF EXISTS `tree_traversal`';
-            $queries[] = 'DROP TABLE IF EXISTS `tree_traversal_with_scope`';
-
+            if (self::$mysqlDbSchemeCreated) {
+                return;
+            } else {
+                self::$mysqlDbSchemeCreated = true;
+            }
             $queries[] = 'CREATE TABLE `tree_traversal` (
                 `tree_traversal_id` int(11) NOT NULL AUTO_INCREMENT,
                 `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
@@ -65,6 +69,7 @@ class TestUtil
                 REFERENCES `tree_traversal_with_scope` (`tree_traversal_id`) 
                 ON DELETE CASCADE ON UPDATE CASCADE';
         } elseif ('pgsql' == TEST_STEFANO_DB_ADAPTER) {
+            // Run this workaround before each test. DbUnit has issue with postgres https://github.com/sebastianbergmann/dbunit/issues/58
             $queries[] = 'DROP TABLE IF EXISTS tree_traversal';
             $queries[] = 'DROP TABLE IF EXISTS tree_traversal_with_scope';
 
