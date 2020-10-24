86315e8b046c4e482bb7bac1dc6acca30a8c7458
Bartko Stefan <bartko.stefan@gmail.com>
2020-10-24 20:08:26 +0200

    ADDED - docker

--- tests/testConfig.php
+++ tests/testConfig.php
@@ -11,20 +11,20 @@ if (file_exists(__DIR__.'/testConfig.local.php')) {
  */
 if ('mysql' == TEST_STEFANO_DB_VENDOR) {
     defined('TEST_STEFANO_DB_HOSTNAME')
-    || define('TEST_STEFANO_DB_HOSTNAME', '127.0.0.1');
+    || define('TEST_STEFANO_DB_HOSTNAME', 'mariadb');
     defined('TEST_STEFANO_DB_DB_NAME')
     || define('TEST_STEFANO_DB_DB_NAME', 'stefano_tests');
     defined('TEST_STEFANO_DB_USER')
-    || define('TEST_STEFANO_DB_USER', 'travis');
+    || define('TEST_STEFANO_DB_USER', 'root');
     defined('TEST_STEFANO_DB_PASSWORD')
     || define('TEST_STEFANO_DB_PASSWORD', '');
 } elseif ('pgsql' == TEST_STEFANO_DB_VENDOR) {
     defined('TEST_STEFANO_DB_HOSTNAME')
-    || define('TEST_STEFANO_DB_HOSTNAME', '127.0.0.1');
+    || define('TEST_STEFANO_DB_HOSTNAME', 'postgres');
     defined('TEST_STEFANO_DB_DB_NAME')
     || define('TEST_STEFANO_DB_DB_NAME', 'stefano_tests');
     defined('TEST_STEFANO_DB_USER')
-    || define('TEST_STEFANO_DB_USER', 'travis');
+    || define('TEST_STEFANO_DB_USER', 'root');
     defined('TEST_STEFANO_DB_PASSWORD')
     || define('TEST_STEFANO_DB_PASSWORD', '');
 } else {
