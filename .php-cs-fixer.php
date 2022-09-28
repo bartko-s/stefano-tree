91a2600694cbc3e53a000217dfe77de7d38b9d12
Bartko Stefan <bartko.stefan@gmail.com>
2022-09-28 07:24:17 +0200

    UPDATE - php cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,8 +4,8 @@ $finder = PhpCsFixer\Finder::create()
     ->in('src')
     ->in('tests');
 
-return PhpCsFixer\Config::create()
-    ->setRules([
+$config = new PhpCsFixer\Config();
+return $config->setRules([
         '@PSR2' => true,
         '@Symfony' => true,
         '@PhpCsFixer' => true,
@@ -13,6 +13,7 @@ return PhpCsFixer\Config::create()
         'declare_strict_types' => true,
         'return_assignment' => false,
         'ordered_class_elements' => false,
+        'php_unit_test_class_requires_covers' => false,
         'no_superfluous_phpdoc_tags' => false,
         'no_useless_else' => false,
         'no_null_property_initialization' => false,
